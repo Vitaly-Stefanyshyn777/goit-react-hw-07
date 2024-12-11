d2c15ece18b47b1b5eaeacddea230f011e39b51c
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2024-12-11 15:49:46 +0200

    додав пакети і тд..

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,30 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  base: '/goit-react-hw-05/',
+  base: "/goit-react-hw-07/",
   plugins: [
     react(),
     visualizer({
-      open: true, 
+      open: true,
     }),
   ],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
+          if (id.includes("node_modules")) {
             return id
               .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
+              .split("node_modules/")[1]
+              .split("/")[0]
               .toString();
           }
         },
       },
       plugins: [visualizer()],
     },
-    chunkSizeWarningLimit: 1000, // Підвищуємо обмеження для попередження про великі частини до 1MB
+    chunkSizeWarningLimit: 1000, 
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   base: "/goit-react-hw-05/", // Замініть на назву вашого репозиторію
-//   plugins: [react()],
-//   build: {
-//     outDir: "dist", // Директорія для зібраних файлів
-//     emptyOutDir: true, // Очищує папку перед новою збіркою
-//   },
-// });
